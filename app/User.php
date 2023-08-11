e2499e3ed1c45166f31b4a67d6e07cacef754896
gabrielteixeira-0814 <gabrielteixeira0814@gmail.com>
2023-08-11 15:59:58 -0300

    Adicionar controllers e APIs

--- app/User.php
+++ app/User.php
@@ -16,7 +16,11 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
+        'type',
+        'is_enabled'
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Route::get('/user', function (Request $request) {
+//     return 'oola';
+// });
+
+    // Users
+    Route::prefix('user')->group(function() {
+            Route::get('/', [UserController::class, 'index'])->name('getListUser');
+            Route::get('/{id}', [UserController::class, 'get'])->name('getUser');
+            Route::post('/', [UserController::class, 'store'])->name('postUser');
+            Route::post('/{id}', [UserController::class, 'update'])->name('putUser');
+            Route::delete('/{id}', [UserController::class, 'delete'])->name('deleteUser');
+    });
